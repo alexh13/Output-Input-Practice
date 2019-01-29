f9bd3e2eb5ad0b2fb165664f9582fe64af352f35
Alex Hughes <hughes.13.alex@gmail.com>
2019-01-28 19:24:32 -0800

    First Commit

---
+++ cmake-build-debug/CMakeFiles/intro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intro.dir/output-intro.cpp.o"
+  "intro.pdb"
+  "intro"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/intro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ output-intro.cpp
@@ -0,0 +1,24 @@
+#include <iostream> // Must be in every program containing cout
+using namespace std;  // cout is defined in namespace std
+
+int main() // where the program will start
+{ // beginning
+    cout << "Hello, World!" << endl; // cout is what will output on the screen
+    cout << "It's sure cold out here!" << endl; // << enl; means end line
+    cout << "Bo said, \"You should wear a jacket!\"" << endl; // use backslashes for double quote strings
+
+    // Our employees make $12 an hour.
+//int hours;
+//cin >> hours; // wait for the user to type in a value and put that value into hoursWorked var
+    int hoursWorked; // created a variable that can be used to store an integer
+    int paycheckAmount;
+    int payrate;
+
+    cout << "Enter hours worked: ";
+    cin >> hoursWorked;
+    cout << "Enter rate of pay: ";
+    cin >> payrate;
+    paycheckAmount = hoursWorked * payrate;
+    cout << "The amount of the paycheck is "
+         << paycheckAmount << " dollars." << endl;
+} // end
